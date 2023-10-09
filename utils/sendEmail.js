fcf6de0e1d0f70eaf5b07b2466abee7ec2cc26f3
anuj123-45 <anuj1358.be20@chikara.edu.in>
2023-10-09 23:30:17 +0530

    updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,27 @@
 const nodeMailer = require("nodemailer");
 
-const sendEmail = async (options) => {
-  const transporter = nodeMailer.createTransport({
-    port: 465,
-    host:'smtp.gmail.com',
-    service: 'gmail',
-    secure:true,
-    
-    auth: {
-      user: 'mymailforabhi@gmail.com',
-      pass: 'passwordForAbhi',
-    },
-  });
+const sendEmail = async(options) =>{
 
-  const mailOptions = {
-    from: 'mymailforabhi@gmail.com',
-    to: options.email,
-    subject: options.subject,
-    text: options.text,
-  };
+    const transporter = nodeMailer.createTransport({
+        host:process.env.SMTPHOST,
+        port:process.env.SMTPPORT,
+        service:process.env.SMTPSERVICE,
+        auth:{
+            user:process.env.SMTPMAIL,
+            pass:process.env.SMTPPASSWORD
+        },
+    });
 
-  await transporter.sendMail(mailOptions);
+    const mailOptions = {
+        from:process.env.SMTPMAIL,
+        to:options.email,
+        subject:options.subject,
+        text:options.message,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+
 };
 
 module.exports = sendEmail;
